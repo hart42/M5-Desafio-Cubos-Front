f64ebaceb63e2467b651af2439437f729bf9102b
Jeremy Paule Pereira <jeremy.workplace@gmail.com>
2022-02-05 14:59:47 -0300

    Cards de Topo Home

--- front-desafio-final/src/pages/home/Home.js
+++ front-desafio-final/src/pages/home/Home.js
@@ -1,7 +1,10 @@
 import './home.css';
 import Menu from '../../components/menu/Menu'
 import Header from '../../components/Header/Header'
-
+import Totais from '../../components/Home/Totais/Totais';
+import cobrancaPaga from '../../assets/home/cobrancaPaga.svg';
+import cobrancaPrevista from '../../assets/home/cobrancaPrevista.svg';
+import cobrancaVencida from '../../assets/home/cobrancaVencida.svg';
 
 function Home() {
     return (
@@ -9,7 +12,24 @@ function Home() {
             <Header titulo='Resumo das cobranças' classname='home-header' />
             <Menu />
             <div className="conteudo">
-                Ola
+                <Totais 
+                valor={30000}
+                titulo={'Cobranças Pagas'}
+                cor={'#EEF6F6'}
+                icone={cobrancaPaga}
+                />
+                <Totais 
+                valor={7000}
+                titulo={'Cobranças Vencidas'}
+                cor={'#FFEFEF'}
+                icone={cobrancaVencida}
+                />
+                <Totais 
+                valor={10000}
+                titulo={'Cobranças Previstas'}
+                cor={'#FCF6DC'}
+                icone={cobrancaPrevista}
+                />
             </div>
         </main>
     );
