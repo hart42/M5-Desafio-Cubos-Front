ab6f8d7622a9c5027d7e61086bcf11da08ccea6e
Guilherme Moreira <guilhermi_silva@hotmail.com>
2022-03-01 17:02:28 -0300

    deletado hook de cobranca

--- src/hooks/useCobrancas.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import CobrancasContext from "../context/CobrancasContext";
-
-function useCobrancas() {
-  return useContext(CobrancasContext);
-}
-
-export default useCobrancas;
